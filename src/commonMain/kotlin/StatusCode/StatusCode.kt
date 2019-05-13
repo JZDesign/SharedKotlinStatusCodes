e92593d8527236885d33dbf324d378eb6e9c29c1
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2019-05-12 19:16:24 -0500

    ifAccepted()

--- src/commonMain/kotlin/StatusCode/StatusCode.kt
+++ src/commonMain/kotlin/StatusCode/StatusCode.kt
@@ -2,4 +2,10 @@ package StatusCode
 
 class StatusCode(var code: Int, var message: String) {
     fun accepted() = this.code == 202
+
+    fun ifAccepted(block: () -> Unit) {
+        if (this.accepted()) {
+            block()
+        }
+    }
 }
